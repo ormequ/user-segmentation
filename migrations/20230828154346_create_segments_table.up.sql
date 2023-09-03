99c30b58c8c973eabd9376a7eb430308cc584e31
ormequ <ilya.belokobylsky@gmail.com>
2023-09-03 14:51:55 +0300

    little fix in migrations

--- migrations/20230828154346_create_segments_table.up.sql
+++ migrations/20230828154346_create_segments_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE segments
 (
-    id   SERIAL PRIMARY KEY,
+    id   BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     slug VARCHAR(255) CONSTRAINT segments_slug_key UNIQUE NOT NULL
 );
 CREATE INDEX ON segments(slug);
--- migrations/20230830100021_create_operations_table.up.sql
+++ migrations/20230830100021_create_operations_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE operations (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT NOT NULL,
     segment_id BIGINT CONSTRAINT operations_segment_id_key NOT NULL REFERENCES segments (id) ON DELETE CASCADE,
     type SMALLINT NOT NULL,
